6b093cc69c3365d6b0b265f789cfe5b0f51e336d
Laxman <2300090230@kluniversity.in>
2025-10-31 08:54:43 +0530

    added icon

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:2420'
+const API_BASE_URL = 'https://smarthivebackend.onrender.com'
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
